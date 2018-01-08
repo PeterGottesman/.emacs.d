fd88a96b7cc3e6d2766903627d2ea24846668473
Peter Gottesman <peter@petergottesman.com>
2018-01-07 21:38:51 -0500

    Migrated to a use-package based config

---
+++ custom.el
@@ -0,0 +1,26 @@
+(provide 'custom)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("1e67765ecb4e53df20a96fb708a8601f6d7c8f02edb09d16c838e465ebe7f51b" default)))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   (quote
+    (multi-term paganini-theme hl-todo magit package-build))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-added ((((type tty)) (:foreground "green"))))
+ '(magit-diff-added-highlight ((((type tty)) (:foreground "LimeGreen"))))
+ '(magit-diff-context-highlight ((((type tty)) (:foreground "default"))))
+ '(magit-diff-file-heading ((((type tty)) nil)))
+ '(magit-diff-removed ((((type tty)) (:foreground "red"))))
+ '(magit-diff-removed-highlight ((((type tty)) (:foreground "IndianRed"))))
+ '(magit-section-highlight ((((type tty)) nil))))
--- init.el
+++ init.el
@@ -13,57 +13,42 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (paganini-theme hl-todo paredit magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq c-default-style "linux"
+      c-basic-offset 4)
+(c-set-offset 'case-label '+)
 
-;;;----------
-;;;modes
-;;;----------
+(setq ess-fancy-comments nil
+      backup-directory-copying t
+      backup-directory-alist '(("." . "~/.emacs.d/backups"))
+      auto-save-file-name-transforms '(("." "~/.emacs.d/backups" t))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t
+      use-package-always-ensure t)
 
-;; Global Highlight mode
+(use-package magit
+  :commands (magit-status magit-blame magit-checkout)
+  :bind ("<f1>" . magit-status))
+
+(use-package hl-todo)
+
 (define-globalized-minor-mode my-hl-todo-mode-global hl-todo-mode
   (lambda () (hl-todo-mode t)))
 
-;; Highlight todos and other keywords
+(use-package paganini-theme
+  :init
+  (load-theme 'paganini t))
+
+(use-package multi-term
+  :bind ("<f12>" . 'multi-term-dedicated-toggle)
+  :config (setq multi-term-program "/bin/bash"
+		multi-term-dedicated-close-back-to-open-buffer-p t))
+
+(delete-selection-mode t)
+(global-linum-mode t)
+(menu-bar-mode -1)
 (my-hl-todo-mode-global t)
 
-;; Delete or replace selection as opposed to at cursor
-(delete-selection-mode t)
-
-;; Show Line Numbers along left side
-(global-linum-mode t)
-
-;; Do not show menu bar along top
-(menu-bar-mode -1)
-
-
-;; Turn off auto indentation of comments in emacs lisp
-(setq ess-fancy-comments nil)
-
-;; Put autosave and backup files in .emacs.d/backups
-(setq backup-directory-alist
-      `((".*" . , "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms
-      `((".*" ,"~/.emacs.d/backups" t)))
-
-;; Set Theme
-(load-theme 'paganini t)
-(custom-set-faces
- ;; other faces
- '(magit-diff-added ((((type tty)) (:foreground "green"))))
- '(magit-diff-added-highlight ((((type tty)) (:foreground "LimeGreen"))))
- '(magit-diff-context-highlight ((((type tty)) (:foreground "default"))))
- '(magit-diff-file-heading ((((type tty)) nil)))
- '(magit-diff-removed ((((type tty)) (:foreground "red"))))
- '(magit-diff-removed-highlight ((((type tty)) (:foreground "IndianRed"))))
- '(magit-section-highlight ((((type tty)) nil))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
